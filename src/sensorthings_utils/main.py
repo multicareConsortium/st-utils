d0052ed0077d865b179de32ff16d17a718975223
Justin Schembri <justin@compound-engineering.com>
2025-03-10 10:14:24 +0100

    adds granular sensor_support

--- src/sensorthings_utils/main.py
+++ src/sensorthings_utils/main.py
@@ -1,8 +1,16 @@
+# standard
+from typing import List, Literal
+# external
+
+# internal
 from sensorthings_utils import netatmo
 
+SupportedSensors = Literal["netatmo",]
 
-def stream_all() -> None:
-    netatmo.stream()
+
+def stream_all(sensor_types: List[SupportedSensors]) -> None:
+    if "netatmo" in sensor_types:
+        netatmo.stream()
 
 
 if __name__ == "__main__":
