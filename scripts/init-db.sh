c2b8e6c800f970f0ecf801e201ae73dacfad5d53
Justin Schembri <justin@compound-engineering.com>
2025-03-19 17:33:34 +0100

    improves database configs
    improves (by replacing) most of the usernames and passwords to reflect those defined in the docker compose file.

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -7,19 +7,27 @@ set -e
 # this is the default entry point for the image
 /usr/local/bin/docker-entrypoint.sh postgres &
 # brief wait untill PG is ready:
-until pg_isready -h database -U sensorthings -d sensorthings; do
+until pg_isready -h database -U ${POSTGRES_USER} -d sensorthings; do
     echo "Waiting for PostgreSQL to be ready"
     sleep 10
 done
 # first auth attempt triggers FROST to create the Basic Auth tables:
 wget --header="Authorization: Basic $(echo -n 'username:password' | base64)" http://web:8080/FROST-Server/v1.1/Datastreams
 # change admin, write and read passwords:
-psql -U sensorthings -d sensorthings <<EOF
+psql -U ${POSTGRES_USER} -d sensorthings <<EOF
 
--- Update password for existing users (example: admin)
+-- Update password for existing users
 UPDATE "USERS"
 SET "USER_PASS" = '${POSTGRES_PASSWORD}'
 WHERE "USER_NAME" IN ('admin', 'write', 'read');
+
+UPDATE "USERS"
+SET "USER_NAME" = '${POSTGRES_USER}'
+WHERE "USER_NAME" = 'admin';
+
+UPDATE "USER_ROLES"
+SET "USER_NAME" = '${POSTGRES_USER}'
+WHERE "USER_NAME" = 'admin';
 
 EOF
 
