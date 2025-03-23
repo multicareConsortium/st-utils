88440d3437af830defc6ff3ccce44fedc5cfeab5
Justin Schembri <justin@compound-engineering.com>
2025-03-23 10:08:55 +0100

    make imports relative

--- web/app.js
+++ web/app.js
@@ -3,7 +3,7 @@ async function fetchThings() {
     updateStatus('Fetching Things...');
     try {
         // Fetch from the Locations API
-        const response = await fetch('http://localhost:8080/FROST-Server/v1.1/Things');
+        const response = await fetch('../FROST-Server/v1.1/Things');
         
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -161,7 +161,7 @@ async function fetchDatastreamsForThing(thingId) {
     updateStatus(`Fetching Datastreams for Thing ID: ${thingId}...`);
     try {
         // First, fetch the Datastreams associated with this Thing
-        const datastreamResponse = await fetch(`http://localhost:8080/FROST-Server/v1.1/Things(${thingId})/Datastreams`);
+        const datastreamResponse = await fetch(`../FROST-Server/v1.1/Things(${thingId})/Datastreams`);
         
         if (!datastreamResponse.ok) {
             throw new Error(`HTTP error! Status: ${datastreamResponse.status}`);
